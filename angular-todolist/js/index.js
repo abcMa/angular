ce12449874c0b0d313c6048a4a447e8652597a97
ma13240973572@sina.com <mf2014ju>
2017-11-06 14:42:45 +0800

    ---------------------------- 换数据结构的版本

--- angular-todolist/js/index.js
+++ angular-todolist/js/index.js
@@ -1,24 +1,21 @@
 var myapp = angular.module("myapp", []);
-myapp.controller("todolist", function ($scope,$timeout) {
-    $scope.vparr = [], $scope.parr = [], $scope.arr = [], $scope.donearr = [];
+myapp.controller("todolist", function ($scope, $timeout) {
+    $scope.donearr = [],$scope.todoitem="";
+    $scope.Arr = {
+        "vp":[],
+        "p":[],
+        "without":[],
+    };
+    //选择优先级的下拉框默认值
+    $scope.selectVal = $scope.Arr.without;
     //优先级
-    $scope.priorityVal = "0";
+    $scope.priorityVal = "without";
     //新增任务
     $scope.add = function () {
         if (!$scope.todoitem || !$scope.todoitem === "") {
             return false;
         }
-        switch ($scope.priorityVal) {
-            case "0":
-                $scope.arr.push({"text": $scope.todoitem, "priority": $scope.priorityVal, "hasdone": false});
-                break;
-            case "1":
-                $scope.vparr.push({"text": $scope.todoitem, "priority": $scope.priorityVal, "hasdone": false});
-                break;
-            case "2":
-                $scope.parr.push({"text": $scope.todoitem, "priority": $scope.priorityVal, "hasdone": false});
-                break;
-        }
+        $scope.Arr[$scope.priorityVal].push({"text": $scope.todoitem, "priority": $scope.priorityVal, "hasdone": false});
         $scope.todoitem = "";
     }
     //完成当前任务
@@ -31,14 +28,14 @@ myapp.controller("todolist", function ($scope,$timeout) {
         arr.splice(i, 1);
     }
     //编辑当前任务
-    $scope.edit = function (item,evt) {
+    $scope.edit = function (item, evt) {
         item.isEditing = true;
         //fix显示input光标不聚焦
         var $editInput = evt.target;
         $scope.editInput = $editInput;
-        $timeout(function(){
+        $timeout(function () {
             angular.element($scope.editInput).next()[0].focus();
-        },100);
+        }, 100);
     }
     //完成编辑
     $scope.editBlur = function (item) {
@@ -46,6 +43,20 @@ myapp.controller("todolist", function ($scope,$timeout) {
     }
     //一键清空所有任务
     $scope.clear = function () {
-        $scope.vparr = [], $scope.parr = [], $scope.arr = [], $scope.donearr = [];
+        angular.forEach($scope.Arr,function (key,val) {
+            $scope.Arr[val]=[];
+        })
+        $scope.donearr = [];
     }
 })
+//处理总数量大于9的
+    .filter("formatPriority", function () {
+        return function (input) {
+            var obj={
+                "vp":"很重要/必须完成",
+                "p":"重要/非必须完成",
+                "without":"无优先级"
+            }
+            return obj[input];
+        }
+    })
