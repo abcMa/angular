d0d14d268f35e2ec6992f8edd3d2a044b77dfecb
ma13240973572@sina.com <mafang@co-mall.com>
2017-12-26 14:16:23 +0800

    index file

--- angular-todolist-directive/js/index.js
+++ angular-todolist-directive/js/index.js
@@ -25,33 +25,6 @@ angular.module("myapp", [])
             $scope.todoitem = "";
         }
 
-        // //完成当前任务
-        // $scope.done = function (i, arr, item) {
-        //     arr.splice(i, 1);
-        //     $scope.donearr.push(item);
-        // }
-
-        // //删除当前任务
-        // $scope.remove = function (i, arr) {
-        //     arr.splice(i, 1);
-        // }
-
-        // //编辑当前任务
-        // $scope.edit = function (item, evt) {
-        //     item.isEditing = true;
-        //     //fix显示input光标不聚焦
-        //     var $editInput = evt.target;
-        //     $scope.editInput = $editInput;
-        //     $timeout(function () {
-        //         angular.element($scope.editInput).next()[0].focus();
-        //     }, 100);
-        // }
-        //
-        // //完成编辑
-        // $scope.editBlur = function (item) {
-        //     item.isEditing = false;
-        // }
-
         //一键清空所有任务
         $scope.clear = function () {
             angular.forEach($scope.Arr, function (key, val) {
